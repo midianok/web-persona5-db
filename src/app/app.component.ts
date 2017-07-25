7b3117fd6db5f36a099a80252164a9a181b96e33
Гусев Илья <midian.ip@gmail.com>
2017-07-25 22:34:32 +0300

    fix search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, ViewChild} from "@angular/core";
 import {PersonasData} from "./data/personas-data";
 import {Persona} from "./model/persona";
 import {AlimentClickedEventArg} from "./model/status-clicked-event-arg";
@@ -9,35 +9,57 @@ import {AlimentClickedEventArg} from "./model/status-clicked-event-arg";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   personaRepository: Array<Persona>;
-   personasToShow: Array<Persona>;
+  @ViewChild('searchInput') searchField: ElementRef;
+  personasRepository: Array<Persona>;
+  personasToShow: Array<Persona>;
+  alimentFilters: Array<AlimentClickedEventArg> = [];
+  alimentFiltersEmpty: boolean = true;
 
-   constructor(){
-     this.personaRepository = PersonasData.getRepository();
-     this.personasToShow = this.personaRepository;
+   constructor() {
+     this.personasRepository = PersonasData.getRepository();
+     this.personasToShow = this.personasRepository;
    }
 
-  filterBySearchRequest(searchStr){
-     if (searchStr.length === 0){
-      this.personasToShow = this.personaRepository;
-    }
-    const result = [];
-    for (const persona of this.personaRepository){
-      if (persona.name.toLowerCase().includes(searchStr.toLocaleLowerCase())){
-        result.push(persona);
-      }
-    }
-    this.personasToShow = result;
+  searchInputChanged() {
+    const searchStr = this.searchField.nativeElement.value;
+    this.personasToShow =
+      this.personasRepository.filter(
+        x => x.name.toLocaleLowerCase().includes(searchStr.toLocaleLowerCase())
+      );
   }
 
-  filterByAliment(clickedAliment: AlimentClickedEventArg){
-    const result = [];
-    for (const persona of this.personasToShow){
-      if (persona[clickedAliment.alimentName] === clickedAliment.alimentValue) {
-        result.push(persona);
-      }
+  filterByAliment(clickedAliment: AlimentClickedEventArg) {
+    const filterAlreadyAdded = this.alimentFilters.some(x =>
+      x.alimentName === clickedAliment.alimentName && x.alimentValue === clickedAliment.alimentValue
+    );
+    if (filterAlreadyAdded) {
+      return;
     }
-    this.personasToShow = result;
+
+    this.personasToShow = this.personasToShow.filter(
+      x => x[clickedAliment.alimentName] === clickedAliment.alimentValue
+    );
+
+    this.alimentFilters.push(clickedAliment);
+    this.alimentFiltersEmpty = false;
   }
 
+  removeFilter(aliment: AlimentClickedEventArg) {
+    this.alimentFilters = this.alimentFilters.filter(
+      x => x !== aliment
+    );
+
+    if (this.alimentFilters.length !== 0){
+      let filteredPersonas =  this.personasRepository;
+      for (const alimentFilter of this.alimentFilters){
+        filteredPersonas = filteredPersonas.filter(
+          x => x[alimentFilter.alimentName] === alimentFilter.alimentValue
+        );
+      }
+      this.personasToShow = filteredPersonas;
+    } else {
+      this.personasToShow = this.personasRepository;
+      this.alimentFiltersEmpty = true;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
 import { PersonaListComponent } from './persona-list/persona-list.component';
 import { OrderbyPipe } from './orderby.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent,
     PersonaListComponent,
     OrderbyPipe
   ],
